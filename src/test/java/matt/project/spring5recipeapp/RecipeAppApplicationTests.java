94dcd2e5d6f111ec22bf6b9efbe747a2b8c2b9a1
Mateusz Siekierka <mat.siekierka96@gmail.com>
2020-11-13 16:34:35 +0100

    init add of project

---
+++ src/test/java/matt/project/spring5recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package matt.project.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
