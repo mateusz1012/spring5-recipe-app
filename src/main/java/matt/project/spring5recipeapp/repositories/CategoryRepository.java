bd68c9dfa56d8c82b8b917a5ecde18ebc8748b3f
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-02 10:34:11 +0100

    Adding Query Method Examples

--- src/main/java/matt/project/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/matt/project/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package matt.project.spring5recipeapp.repositories;
 import matt.project.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/matt/project/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/matt/project/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package matt.project.spring5recipeapp.repositories;
 import matt.project.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
