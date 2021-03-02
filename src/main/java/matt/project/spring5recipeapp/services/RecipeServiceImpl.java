ef8355a7ec8d948f6fdb02793cffc77b33147986
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-02 11:40:54 +0100

    Adding RecipeBootstrap and more

---
+++ src/main/java/matt/project/spring5recipeapp/services/RecipeServiceImpl.java
@@ -0,0 +1,26 @@
+package matt.project.spring5recipeapp.services;
+
+import matt.project.spring5recipeapp.domain.Recipe;
+import matt.project.spring5recipeapp.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
