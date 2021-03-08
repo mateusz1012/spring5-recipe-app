1d2574d49ab5d82497c1705aec9ea3a27289a503
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-08 14:16:07 +0100

    adding junit test

---
+++ src/test/java/matt/project/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package matt.project.spring5recipeapp.domain;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
