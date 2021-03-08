ccb7e2d30245ac9974ba7712f047a2d700eaac13
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-08 14:29:26 +0100

    adding mockito example

---
+++ src/test/java/matt/project/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package matt.project.spring5recipeapp.services;
+
+import matt.project.spring5recipeapp.domain.Recipe;
+import matt.project.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class RecipeServiceImplTest {
+    
+    RecipeServiceImpl recipeService;
+    
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        Mockito.lenient().when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
