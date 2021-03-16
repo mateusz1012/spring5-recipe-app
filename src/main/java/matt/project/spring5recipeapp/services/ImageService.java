48201dbdd7109dcb9b591326aab42c33c52991bf
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-16 13:41:51 +0100

    adding image upload - front end

---
+++ src/main/java/matt/project/spring5recipeapp/services/ImageService.java
@@ -0,0 +1,1 @@
+package matt.project.spring5recipeapp.services;import org.springframework.web.multipart.MultipartFile;public interface ImageService {    void saveImageFile(Long recipeId, MultipartFile file);}
