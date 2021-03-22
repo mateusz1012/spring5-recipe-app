9d3135ccc1a3867291c199b7ae28a7bbef7da308
Mateusz Siekierka <mat.siekierka96@gmail.com>
2021-03-22 13:40:12 +0100

    Adding exception handler

--- src/main/java/matt/project/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/matt/project/spring5recipeapp/controllers/RecipeController.java
@@ -2,13 +2,13 @@ package matt.project.spring5recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import matt.project.spring5recipeapp.commands.RecipeCommand;
+import matt.project.spring5recipeapp.exceptions.NotFoundException;
 import matt.project.spring5recipeapp.services.RecipeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -55,5 +55,17 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
